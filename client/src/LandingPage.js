9e53395152ca11dd7ac198454327a80cfedd8489
Amali M <amalibmchie@gmail.com>
2024-09-23 16:38:34 -0400

    Update LandingPage.js

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -6,45 +6,41 @@ const LandingPage = () => {
 
   return (
     <div>
-      <header>
-        <nav>
-          <div className="rightNav">
-          <button onClick={() => navigate('/')}>
-            <img></img>
-          </button>
-          </div>
-          <div className="leftNav">
-            <button onClick={() => navigate('/login')}>Login</button>
-            <button onClick={() => navigate('/create-account')}>Create Account</button>
-          </div>
-        </nav>
-      </header>
-      <body>
-      <div class="slider">
-            <figure>
-                <div class="slide">
-                    <p>Image One</p>
-                    <img src={"./tempAssets/adamrotter_loafc4_wwwadamrotterpl-1.jpg"}></img>
-                </div>
-                <div class="slide">
-                    <p>Image Two</p>
-                    <img src="tempAssets/1-Rashed-AlShashai-A-Concise-Passage-installation-view-at-Desert-X-AlUla-photo-Lance-Gerber-courtesy-the-artist-RCU-and-Desert-X-scaled.jpg"></img>
-                </div>
-                <div class="slide">
-                    <p>Image Three</p>
-                    <img src="tempAssets/content_aziz-acharki-290990.jpg"></img>
-                </div>
-                <div class="slide">
-                    <p>Image Four</p>
-                    <img src="tempAssets/Cover21.jpg"></img>
-                </div>
-                <div class="slide">
-                    <p>Image Five</p>
-                    <img src="tempAssets/shutterstock_189352979.jpg"></img>
-                </div>
-            </figure>
+      <nav>
+        <div className="rightNav">
+        <button onClick={() => navigate('/')}>
+          <img></img>
+        </button>
         </div>
-      </body>
+        <div className="leftNav">
+          <button onClick={() => navigate('/login')}>Login</button>
+          <button onClick={() => navigate('/create-account')}>Create Account</button>
+        </div>
+      </nav>
+      <div class="slider">
+          <figure>
+              <div class="slide">
+                  <p>Image One</p>
+                  <img src={"./tempAssets/adamrotter_loafc4_wwwadamrotterpl-1.jpg"}></img>
+              </div>
+              <div class="slide">
+                  <p>Image Two</p>
+                  <img src="tempAssets/1-Rashed-AlShashai-A-Concise-Passage-installation-view-at-Desert-X-AlUla-photo-Lance-Gerber-courtesy-the-artist-RCU-and-Desert-X-scaled.jpg"></img>
+              </div>
+              <div class="slide">
+                  <p>Image Three</p>
+                  <img src="tempAssets/content_aziz-acharki-290990.jpg"></img>
+              </div>
+              <div class="slide">
+                  <p>Image Four</p>
+                  <img src="tempAssets/Cover21.jpg"></img>
+              </div>
+              <div class="slide">
+                  <p>Image Five</p>
+                  <img src="tempAssets/shutterstock_189352979.jpg"></img>
+              </div>
+          </figure>
+      </div>
     </div>
   );
 };
