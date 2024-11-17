0dc08b886d41c9c27e49c27f4cc1a963d8651e91
Steven1543 <109391612+Steven1543@users.noreply.github.com>
2024-11-17 15:32:14 -0500

    Actually pushing the changes

---
+++ client/src/PrintButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrintButton = () => {
+  const handlePrint = () => {
+    window.print();
+  };
+
+  return <button onClick={handlePrint}>Print</button>;
+};
+
+export default PrintButton;
